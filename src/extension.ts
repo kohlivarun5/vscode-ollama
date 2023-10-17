a8e20b545021e12f7f4a246199029d579006501b
Varun Kohli <kohli.varun5@gmail.com>
2023-10-16 20:23:57 -0400

    .

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Range } from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -20,6 +21,85 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	const provider: vscode.InlineCompletionItemProvider = {
+		async provideInlineCompletionItems(document, position, context, token) {
+			console.log(['provideInlineCompletionItems triggered',position,context,token]);
+			const regexp = /\/\/ \[(.+?),(.+?)\)(.*?):(.*)/;
+			console.log(document.getText());
+			if (position.line < 0) {
+				return;
+			}
+
+			const result: vscode.InlineCompletionList = {
+				items: [],
+				//commands: [],
+			};
+
+			result.items.push({
+				insertText: new vscode.SnippetString("Test"),
+				range: new Range(position.line, position.character, position.line, position.character),
+				//completeBracketPairs,
+			});
+
+			let offset = 1;
+			while (offset > 0) {
+				if (position.line - offset < 0) {
+					break;
+				}
+				console.log(document.getText());
+				const lineBefore = document.lineAt(position.line - offset).text;
+				const matches = lineBefore.match(regexp);
+				if (!matches) {
+					break;
+				}
+				offset++;
+
+				const start = matches[1];
+				const startInt = parseInt(start, 10);
+				const end = matches[2];
+				const endInt =
+					end === '*'
+						? document.lineAt(position.line).text.length
+						: parseInt(end, 10);
+				const flags = matches[3];
+				const completeBracketPairs = flags.includes('b');
+				const isSnippet = flags.includes('s');
+				const text = matches[4].replace(/\\n/g, '\n');
+
+				result.items.push({
+					insertText: isSnippet ? new vscode.SnippetString(text) : text,
+					range: new Range(position.line, startInt, position.line, endInt),
+					//completeBracketPairs,
+				});
+			}
+
+			//if (result.items.length > 0) {
+			//	result.commands!.push({
+			//		command: 'demo-ext.command1',
+			//		title: 'My Inline Completion Demo Command',
+			//		arguments: [1, 2],
+			//	});
+			//}
+			return result;
+		},
+
+		//handleDidShowCompletionItem(completionItem: vscode.InlineCompletionItem): void {
+		//	console.log('handleDidShowCompletionItem');
+		//},
+
+		/**
+		 * Is called when an inline completion item was accepted partially.
+		 * @param acceptedLength The length of the substring of the inline completion that was accepted already.
+		 */
+		//handleDidPartiallyAcceptCompletionItem(
+		//	completionItem: vscode.InlineCompletionItem,
+		//	acceptedLength: number
+		//): void {
+		//	console.log('handleDidPartiallyAcceptCompletionItem');
+		//},
+	};
+	vscode.languages.registerInlineCompletionItemProvider({ pattern: '**' }, provider);
 }
 
 // This method is called when your extension is deactivated
